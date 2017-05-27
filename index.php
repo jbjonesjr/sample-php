d32253f8387e359794118a079782d87d0213367d
Jamie Jones <jbjonesjr@github.com>
2017-05-27 15:15:07 -0400

    draw out some other items

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 <title>Single page example</title>
 </head>
 <body>
+  <h1>Start of something good?</h1>
 <?php
   echo("Basic page doing basic things!!");
 ?>
+  <hr/>
+  <p>Other stuff down here</p>
 </body>
 </html>
