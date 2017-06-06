d88080e3b373bcfb37ba1151c7154b9272f5a19f
Jamie Jones <jbjonesjr@github.com>
2017-06-06 08:59:43 -0400

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   <p>Branch:test-branch</p>
   <p>Code Climate:enabled</p>
   <p>Now with push &amp; statuses correctly, and status webhook</p>
-  <p>Adding more status, with permissions now</p>
+  <p>Adding more status, with permissions now, and the right sha</p>
   <?php echo('yes'); ?>
 </body>
 </html>
