754b165975d272bdefcd7e97403d3114eea0bc43
Jamie Jones <jbjonesjr@github.com>
2017-06-07 01:28:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,6 @@
   <p>Adding more status, with permissions now, and the right sha with partial completition. now with proper plumbing</p>
   <p>Code Climate back?</p>
   <?php echo('yes'); ?>
-  <p>required status</p>
+  <p>await required status</p>
 </body>
 </html>
