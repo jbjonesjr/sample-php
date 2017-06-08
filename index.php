26f39fc060f8cf1a5fbce1981ed44024f04d5696
Jamie Jones <jbjonesjr@github.com>
2017-06-08 00:55:44 -0400

    fixing unit test failure?

--- index.php
+++ index.php
@@ -10,6 +10,6 @@
   <hr/>
   <p>Other stuff down here</p>
   <p>Branch: the-color-purple</p>
-  <p>No specific body details needed</p> fail1
+  <p>No specific body details needed</p> fail12
 </body>
 </html>
