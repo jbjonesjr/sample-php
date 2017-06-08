7aa21fb9d97a5bbb04635bac523b95ababa3fe07
Jamie Jones <jbjonesjr@github.com>
2017-06-08 00:27:23 -0400

    Create index.php

--- index.php
+++ index.php
@@ -16,6 +16,6 @@
   <p>Code Climate back?</p>
   <?php echo('yes'); ?>
   <p>await required status with reponame &amp; permissions for try/catch with 404 detection.testing</p>
-<p>pull w/ heads now with better query</p>
+<p>pull w/ heads now with better query weird order</p>
 </body>
 </html>
