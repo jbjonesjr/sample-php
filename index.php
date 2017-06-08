481480ed4ff66f7aeb7567187e43f5343fed7758
Jamie Jones <jbjonesjr@github.com>
2017-06-08 00:57:01 -0400

    fixing failing test again

--- index.php
+++ index.php
@@ -10,6 +10,6 @@
   <hr/>
   <p>Other stuff down here</p>
   <p>Branch: the-color-purple</p>
-  <p>No specific body details needed</p> fail12
+  <p>No specific body details needed</p>
 </body>
 </html>
